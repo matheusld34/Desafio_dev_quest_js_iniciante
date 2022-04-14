ceb7cc33ec91ab06dc8922d200000170fc2d6b11
Matheus Lopes <matheus_lopes151@hotmail.com>
2022-04-14 19:22:18 -0300

    Desafio_1

---
+++ Desafio_1/index.js
@@ -0,0 +1,32 @@
+function soma(numero1, numero2) {
+
+    return numero1 + numero2
+}
+
+function subtracao(numero1, numero2) {
+
+    return numero1 - numero2
+}
+
+function multiplicacao(numero1, numero2) {
+
+    return numero1 * numero2
+}
+
+function divisao(numero1, numero2) {
+
+    return numero1 / numero2
+
+}
+
+let resultadoSoma = soma(8, 4)
+console.log('O resultado da soma é ' + resultadoSoma)
+
+let resultadoSubtracao = subtracao(8, 4)
+console.log('O resultado da subtração é ' + resultadoSubtracao)
+
+let resultadoMultiplicacao = multiplicacao(8, 4)
+console.log('O resultado da multiplicação é ' + resultadoMultiplicacao)
+
+let resultadoDivisao = divisao(8, 4)
+console.log('O resultado da soma é ' + resultadoDivisao)
